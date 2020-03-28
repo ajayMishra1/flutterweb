4a2d5913237ec50c8d8a90f292f63052d60cddf8
ajay mishra <mishraajay95124@gmail.com>
2020-03-28 19:58:51 +0530

    28 march 2020 8 pm

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:simple_animations/simple_animations/animation_progress.dart';
 import 'package:simple_animations/simple_animations/controlled_animation.dart';
 import 'package:simple_animations/simple_animations/multi_track_tween.dart';
-import 'package:simple_animations/simple_animations/rendering.dart';
+import 'dart:html' as html;
 
 void main() => runApp(ParticleApp());
 
@@ -12,6 +11,7 @@ class ParticleApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: "Flutter web demo by AjaY...",
       home: Scaffold(
         body: ParticleBackgroundPage(),
       ),
@@ -25,108 +25,11 @@ class ParticleBackgroundPage extends StatelessWidget {
     return Stack(
       children: <Widget>[
         Positioned.fill(child: AnimatedBackground()),
-        Positioned.fill(child: Particles(30)),
+      //  Positioned.fill(child: Particles(30)),
         Positioned.fill(child: CenteredText()),
       ],
     );
   }
-}
-
-class Particles extends StatefulWidget {
-  final int numberOfParticles;
-
-  Particles(this.numberOfParticles);
-
-  @override
-  _ParticlesState createState() => _ParticlesState();
-}
-
-class _ParticlesState extends State<Particles> {
-  final Random random = Random();
-
-  final List<ParticleModel> particles = [];
-
-  @override
-  void initState() {
-    List.generate(widget.numberOfParticles, (index) {
-      particles.add(ParticleModel(random));
-    });
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Rendering(
-      startTime: Duration(seconds: 30),
-      onTick: _simulateParticles,
-      builder: (context, time) {
-        return CustomPaint(
-          painter: ParticlePainter(particles, time),
-        );
-      },
-    );
-  }
-
-  _simulateParticles(Duration time) {
-    particles.forEach((particle) => particle.maintainRestart(time));
-  }
-}
-
-class ParticleModel {
-  Animatable tween;
-  double size;
-  AnimationProgress animationProgress;
-  Random random;
-
-  ParticleModel(this.random) {
-    restart();
-  }
-
-  restart({Duration time = Duration.zero}) {
-    final startPosition = Offset(-0.2 + 1.4 * random.nextDouble(), 1.2);
-    final endPosition = Offset(-0.2 + 1.4 * random.nextDouble(), -0.2);
-    final duration = Duration(milliseconds: 3000 + random.nextInt(6000));
-
-    tween = MultiTrackTween([
-      Track("x").add(
-          duration, Tween(begin: startPosition.dx, end: endPosition.dx),
-          curve: Curves.easeInOutSine),
-      Track("y").add(
-          duration, Tween(begin: startPosition.dy, end: endPosition.dy),
-          curve: Curves.easeIn),
-    ]);
-    animationProgress = AnimationProgress(duration: duration, startTime: time);
-    size = 0.2 + random.nextDouble() * 0.4;
-  }
-
-  maintainRestart(Duration time) {
-    if (animationProgress.progress(time) == 1.0) {
-      restart(time: time);
-    }
-  }
-}
-
-class ParticlePainter extends CustomPainter {
-  List<ParticleModel> particles;
-  Duration time;
-
-  ParticlePainter(this.particles, this.time);
-
-  @override
-  void paint(Canvas canvas, Size size) {
-    final paint = Paint()..color = Colors.white.withAlpha(50);
-
-    particles.forEach((particle) {
-      var progress = particle.animationProgress.progress(time);
-      final animation = particle.tween.transform(progress);
-      final position =
-          Offset(animation["x"] * size.width, animation["y"] * size.height);
-      canvas.drawCircle(position, size.width * 0.2 * particle.size, paint);
-    });
-  }
-
-  @override
-  bool shouldRepaint(CustomPainter oldDelegate) => true;
 }
 
 class AnimatedBackground extends StatelessWidget {
@@ -163,12 +66,58 @@ class CenteredText extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        "Welcome to Flutter for web",
-        style: TextStyle(color: Colors.white, fontWeight: FontWeight.w200),
-        textScaleFactor: 4,
+    return 
+    Container(
+      padding: EdgeInsets.all(20),
+      child : Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: <Widget>[
+      Text(
+        "Hi there! My name is",
+        textScaleFactor: 2,
+        style: TextStyle(color: Colors.orange),
       ),
+      Text(
+        "Mishra ajay",
+        textScaleFactor: 3,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      SizedBox(
+        height: 10,
+      ),
+      Text(
+        "I am an iOS Developer. \nI have 3 years or experience in iPhone development. \nI am working on One click IT Consultancy."
+
+        ,      
+          softWrap: true,
+        textScaleFactor: 1.5,
+        style: TextStyle(color: Colors.white70),
+      ),
+      SizedBox(
+        height: 20,
+      ),
+    
+          RaisedButton(
+            shape: StadiumBorder(),
+            child: Text("One Click",  style: TextStyle(color: Colors.white70)),
+            color: Colors.red,
+            onPressed: () {
+              html.window
+                  .open("https://www.oneclickitsolution.com/", "Home page");
+            },
+            padding: EdgeInsets.all(10),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+      
+     
+    ],
+  )
     );
+
   }
 }
